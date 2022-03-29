086d202b89bb3fe3116f0ee5f18d12363e9783ad
SRataj1005 <100698720+SRataj1005@users.noreply.github.com>
2022-03-29 12:12:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    int a = 5;
+    for(int i=0;i<=a;i++)
+      {
+        for(int j = 0; j < i;j++)
+          {
+            System.out.print("*");
+          }
+        System.out.println(" ");
+      }
+      
+  }
+}
